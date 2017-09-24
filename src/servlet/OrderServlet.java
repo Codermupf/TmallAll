a7c6fc4ed7dbf8fcbeb30e21b6d29a82c1882869
OovEver <mupengfei6688@163.com>
2017-09-25 00:10:08 +0800

    添加订单显示界面功能

---
+++ src/servlet/OrderServlet.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import bean.Order;
+import dao.OrderDAO;
+import util.Page;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by OovEver on 2017/9/25.
+ */
+@WebServlet(name = "OrderServlet")
+public class OrderServlet  extends BaseBackServlet {
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        List<Order> os = orderDAO.list(page.getStart(),page.getCount());
+        orderItemDAO.fill(os);
+        int total = orderDAO.getTotal();
+        page.setTotal(total);
+
+        request.setAttribute("os", os);
+        request.setAttribute("page", page);
+
+        return "admin/listOrder.jsp";
+    }
+
+    public String delivery(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int   id = Integer.parseInt(request.getParameter("id"));
+        Order o  = orderDAO.get(id);
+        o.setDeliveryDate(new Date());
+        o.setStatus(OrderDAO.waitConfirm);
+        orderDAO.update(o);
+        return "@admin_order_list";
+    }
+}
