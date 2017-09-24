2685723a34927517aa2950843e276c43f69c0334
OovEver <mupengfei6688@163.com>
2017-09-24 17:37:18 +0800

    更新分类增删查改功能

--- src/filter/BackServletFilter.java
+++ src/filter/BackServletFilter.java
@@ -15,25 +15,28 @@ import java.io.IOException;
 public class BackServletFilter implements Filter {
     public void destroy() {
     }
-
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+//        HttpServletRequest是ServletRequest的子类
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-        String contextPath=request.getServletContext().getContextPath();
+//        解决相对路径问题，返回站点的根路径，
+        String contextPath=request.getServletContext().getContextPath ();
+//        通过request.getRequestURI()取出访问的uri: /tmall/admin_category_list
         String uri = request.getRequestURI();
         //        得到工程名
         uri =StringUtils.remove(uri, contextPath);
         if(uri.startsWith("/admin_")){
+
+
             String servletPath = StringUtils.substringBetween(uri,"_", "_") + "Servlet";
             String method = StringUtils.substringAfterLast(uri,"_" );
             request.setAttribute("method", method);
+//            ("/" + servletPath)="/categoryServlet"
             req.getRequestDispatcher("/" + servletPath).forward(request, response);
             return;
         }
-
         chain.doFilter(request, response);
     }
-
     public void init(FilterConfig config) throws ServletException {
 
     }
